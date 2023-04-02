3e703857a08a2ee8a1bf81223ad532a2a143d426
charlie <cj2clifton@gmail.com>
2023-04-02 15:25:48 -0500

    work for 4/2

---
+++ app/src/main/java/com/example/replacement/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.replacement
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.replacement.ui.theme.ReplacementTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ReplacementTheme {
+                Surface(Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Text("test", color = MaterialTheme.colors.primary)
+                }
+                }
+            }
+        }
+    }
+
---
+++ app/src/main/java/com/example/replacement/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.replacement.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val black432 = Color(0xFF262626)
+val red564 = Color(0xFFed3118)
